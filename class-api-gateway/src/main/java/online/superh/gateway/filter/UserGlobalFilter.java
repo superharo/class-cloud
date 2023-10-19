c371e8187eac106c38669218b014aeb91f4419f9
haro <superh.sun@hi-finance.com.cn>
2023-10-19 13:42:59 +0800

    整合sleuth + zipking 链路追踪

--- class-api-gateway/src/main/java/online/superh/gateway/filter/UserGlobalFilter.java
+++ class-api-gateway/src/main/java/online/superh/gateway/filter/UserGlobalFilter.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -15,7 +14,7 @@ import reactor.core.publisher.Mono;
  * @description:
  * @date: 2023-10-18 15:04
  */
-@Component
+// @Component
 public class UserGlobalFilter implements GlobalFilter, Ordered {
 
 
---
+++ class-api-gateway/src/main/java/online/superh/gateway/filter/package-info.java
@@ -0,0 +1,4 @@
+/*
+注意：⽹关不要加太多业务逻辑，否则会影响性能，务必记住
+ */
+package online.superh.gateway.filter;
